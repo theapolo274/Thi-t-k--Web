48a8bb8c4d189cee66c76d58202e0e01bf5e7fe5
Thế <125851661+theapolo274@users.noreply.github.com>
2024-01-13 11:58:14 +0700

    Add files via upload

---
+++ Buổi 5/bai3.js
@@ -0,0 +1,10 @@
+function ktra(x){
+    if(x >= 1 && x <= 10){
+        if(x==a){
+            alert("Chúc mừng! Bạn đã đoán đúng số.");
+        }
+        else{
+            alert("Số bạn nhập không đúng. Hãy thử lại.");
+        } 
+    }
+}
